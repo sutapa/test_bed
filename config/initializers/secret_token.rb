19a058bb6bddaa93496ef80e25a6efcc2d6f7cba
Sutapa Maity <sutapa.maity@sv.cmu.edu>
2011-09-24 18:08:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBed::Application.config.secret_token = 'da087b6f477f1bde634e19a0e0df8ce8f7249bbffd0fa321bc7100ec7eb318729cd3a2fd89970e2b4f626fd7ec908a2e269cd007c654470df6763dba070df430'
